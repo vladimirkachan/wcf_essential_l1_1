b8bfeb13b3f45bd942ec49fbb0a01e7f789a39ac
vladimirkachan <vladimirkachan0@gmail.com>
2021-06-23 14:58:34 +0300

    Client and Server application done

---
+++ wcf_essential_l1_1.Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wcf_essential_l1_1.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "SERVER";
+            Uri address = new("http://localhost:4000/IContract");
+            BasicHttpBinding binding = new();
+            Type contract = typeof(IContract);
+            using ServiceHost host = new(typeof(Service));
+            host.AddServiceEndpoint(contract, binding, address);
+            host.Open();
+            Console.WriteLine("The application is ready to receive messages.");
+            Console.ReadKey();
+        }
+    }
+}
